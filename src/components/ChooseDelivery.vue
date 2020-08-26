8b31d438fdfbfdb30f088ca72fee56f156d9eb24
David Andersson <lejonmanen@gmail.com>
2020-08-26 10:31:31 +0200

    Exercise 2.3

---
+++ src/components/ChooseDelivery.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+    <div class="">
+        <label class="text">
+            <input type="checkbox"
+                @change="handleChange" />
+            {{ text }}
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            text: 'Leverans om 5 dagar'
+        }
+    },
+    methods: {
+        handleChange(event) {
+            const isChecked = event.target.checked;
+            if( isChecked ) {
+                this.text = 'Valde express, leverans om 3 dagar';
+            } else {
+                this.text = 'Leverans om 5 dagar';
+            }
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ tests/unit/ChooseDelivery.spec.js
@@ -0,0 +1,27 @@
+import { shallowMount } from '@vue/test-utils'
+import ChooseDelivery from '@/components/ChooseDelivery.vue'
+
+/*3 Skapa en komponent med en kryssruta. Komponenten ska initialt visa texten "Leverans om 5 dagar". När man kryssar i rutan ska texten ändras till "Valde express, leverans om 3 dagar". Använd en change event i stället för click event.
+*/
+describe('ChooseDelivery.vue', () => {
+
+    it('should display certain text when rendered', () => {
+        const initial = "Leverans om 5 dagar";
+        const wrapper = shallowMount(ChooseDelivery);
+
+        const text = wrapper.find('.text').text();
+
+        expect(text).toBe(initial);
+    })
+
+    it('should display other text when checked', async () => {
+        const expected = 'Valde express, leverans om 3 dagar';
+        const wrapper = shallowMount(ChooseDelivery);
+        const checkbox = wrapper.find('input');
+
+        await checkbox.setChecked(true)
+        const text = wrapper.find('.text').text();
+
+        expect(text).toBe(expected);
+    })
+})
